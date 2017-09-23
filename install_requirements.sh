8627df858c2adf84bc1de8cc01466f0aca3e2f1c
Larry Neal <nealla@lwneal.com>
2017-09-23 00:38:06 -0700

    Add requirements for dataset downloads

--- install_requirements.sh
+++ install_requirements.sh
@@ -5,7 +5,9 @@ TORCH_URL="http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-man
 
 nvidia-smi
 
+pip install requests
 pip install numpy
+pip install scipy
 pip install Pillow
 pip install tqdm
 pip install scipy
