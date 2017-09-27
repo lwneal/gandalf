da8095bcc7ff937f1c5e04321c2d9589dfdab4ba
Larry Neal <nealla@lwneal.com>
2017-09-27 14:55:12 -0700

    Add new generator28 et al for MNIST
    These networks match the 28x28 size of MNIST and should train faster

--- network_definitions.py
+++ network_definitions.py
@@ -12,6 +12,100 @@ def weights_init(m):
         m.bias.data.fill_(0)
 
 
+class generator28(nn.Module):
+    def __init__(self, latent_size=10):
+        super(self.__class__, self).__init__()
+        Z = latent_size
+        # in_channels, out_channels, kernel_size, stride, padding
+        self.conv1 = nn.ConvTranspose2d(     Z,    256, 7, 1, 0, bias=False)
+        self.conv2 = nn.ConvTranspose2d(   256,    128, 4, 2, 1, bias=False)
+        self.conv3 = nn.ConvTranspose2d(   128,      3, 4, 2, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(256)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = x.unsqueeze(-1).unsqueeze(-1)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, True)(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, True)(x)
+        x = self.conv3(x)
+        x = nn.Sigmoid()(x)
+        return x
+
+
+class discriminator28(nn.Module):
+    def __init__(self, latent_size=100):
+        super(self.__class__, self).__init__()
+        # 3x28x28
+        self.conv1 = nn.Conv2d(3,       64,      4, 2, 1, bias=False)
+        # 64x14x14
+        self.conv2 = nn.Conv2d(64,      128,     4, 2, 1, bias=False)
+        # 128x7x7
+        self.conv3 = nn.Conv2d(128,     256,     4, 2, 1, bias=False)
+        # 256x3x3
+        self.conv4 = nn.Conv2d(256,     1,     3, 1, 0, bias=False)
+        # 1x1x1
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv4(x)
+        # Global average pooling
+        x = x.mean(-1).mean(-1)
+        return x.view(-1, 1).squeeze(1)
+
+
+class encoder28(nn.Module):
+    def __init__(self, latent_size=100):
+        super(self.__class__, self).__init__()
+        # 3x28x28
+        self.conv1 = nn.Conv2d(3,       64,      4, 2, 1, bias=False)
+        # 64x14x14
+        self.conv2 = nn.Conv2d(64,      128,     4, 2, 1, bias=False)
+        # 128x7x7
+        self.conv3 = nn.Conv2d(128,     256,     4, 2, 1, bias=False)
+        # 256x3x3
+        self.conv4 = nn.Conv2d(256,     latent_size,     3, 1, 0, bias=False)
+        # 1x1x1
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv4(x)
+        return x.squeeze()
+
+
 class generatorReLU64(nn.Module):
     def __init__(self, latent_size=100):
         super(self.__class__, self).__init__()
