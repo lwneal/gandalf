4bed33ce47a6436ab67952b491c3df41095cea1d
Larry Neal <nealla@lwneal.com>
2017-09-27 17:30:07 -0700

    Include average reconstruction loss in evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import torch
 from torch.autograd import Variable
@@ -6,16 +7,24 @@ from torch.nn.functional import nll_loss
 import imutil
 
 
+def to_np(v):
+    return v.data.cpu().numpy()
+
+
 def evaluate_classifier(networks, dataloader, **options):
     netE = networks['encoder']
+    netG = networks['generator']
     netC = networks['classifier']
     result_dir = options['result_dir']
     batch_size = options['batch_size']
     image_size = options['image_size']
     latent_size = options['latent_size']
 
+    latent_points = []
     correct = 0
     total = 0
+    mae = 0
+    mse = 0
     
     for i, (images, labels) in enumerate(dataloader):
         images = Variable(images)
@@ -26,12 +35,24 @@ def evaluate_classifier(networks, dataloader, **options):
         correct += sum(predicted.data == labels)
         total += len(predicted)
 
+        z = netE(images)
+        latent_points.extend(z)
+        reconstructed = netG(z)
+        mae += torch.mean(torch.abs(reconstructed - images))
+        mse += torch.mean((reconstructed - images) ** 2)
         print("Accuracy: {:.4f} ({: >12} / {: <12} correct)".format(float(correct) / total, correct, total))
+
+    mse = float(to_np(mse / i)[0])
+    mae = float(to_np(mae / i)[0])
+    print("Reconstruction per-pixel MSE: {}".format(mse))
+    print("Reconstruction per-pixel MAE: {}".format(mae))
 
     return {
         options['fold']: {
             'correct': correct,
             'total': total,
+            'mse': mse,
+            'mae': mae,
             'accuracy': float(correct) / total,
         }
     }
