c7da61de44d168e25b0f49bfa1480160698a80f0
Larry Neal <nealla@lwneal.com>
2017-10-03 14:50:52 -0700

    Fix crash on odd-sized final batch
    For fixed-norm GAN encoder

--- network_definitions.py
+++ network_definitions.py
@@ -501,7 +501,7 @@ class encoder32sphere(nn.Module):
         x = x.squeeze()
         x = self.fc1(x)
         xnorm = torch.norm(x, p=2, dim=1).detach()
-        xnorm = xnorm.expand(1, self.batch_size)
+        xnorm = xnorm.expand(1, x.size()[0])
         xnorm = xnorm.transpose(1,0)
         x = x.div(xnorm)
         return x
