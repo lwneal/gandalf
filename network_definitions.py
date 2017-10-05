65b82cc36ed8e2939ef0e199827b9321f2377c3f
Larry Neal <nealla@lwneal.com>
2017-10-05 08:49:48 -0700

    Add encoder28dropout
    Trying to get better classification results on MNIST

--- network_definitions.py
+++ network_definitions.py
@@ -107,6 +107,41 @@ class encoder28(nn.Module):
         return x.squeeze(-1).squeeze(-1)
 
 
+class encoder28dropout(nn.Module):
+    def __init__(self, latent_size=100, **kwargs):
+        super(self.__class__, self).__init__()
+        # 3x28x28
+        self.conv1 = nn.Conv2d(3,       64,      4, 2, 1, bias=False)
+        # 64x14x14
+        self.conv2 = nn.Conv2d(64,      128,     4, 2, 1, bias=False)
+        # 128x7x7
+        self.conv3 = nn.Conv2d(128,     256,     4, 2, 1, bias=False)
+        # 256x3x3
+        self.conv4 = nn.Conv2d(256,     latent_size,     3, 1, 0, bias=False)
+        self.conv4_drop = nn.Dropout2d()
+        # 1x1x1
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv4(x)
+        x = self.conv4_drop(x)
+        return x.squeeze(-1).squeeze(-1)
+
+
 class generator32(nn.Module):
     def __init__(self, latent_size=100, **kwargs):
         super(self.__class__, self).__init__()
