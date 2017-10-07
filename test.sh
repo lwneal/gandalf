a17b245d1079f6e235318c6db7512d9cdb391edb
Larry Neal <nealla@lwneal.com>
2017-10-06 23:22:43 -0700

    Add example.py to test script

--- test.sh
+++ test.sh
@@ -21,7 +21,10 @@ experiments/train_aac.py \
     --result_dir /mnt/results/mnist_28x28 \
     --dataset /mnt/data/mnist.dataset \
     --image_size 28 \
+    --latent_size 10 \
     --encoder encoder28 \
     --generator generator28 \
     --discriminator discriminator28 \
     --epochs 1
+
+experiments/example.py --result_dir /mnt/results/mnist_28x28
