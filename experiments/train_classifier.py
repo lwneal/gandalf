eff3469a07cc19f139d710360dad78e6283c9f7e
Larry Neal <nealla@lwneal.com>
2017-10-06 22:24:04 -0700

    Save results for train_classifier
    The semisupervised learning experiment will now save as another
    fold in results_dir/eval_epoch_%04d.json

--- experiments/train_classifier.py
+++ experiments/train_classifier.py
@@ -11,7 +11,7 @@ parser.add_argument('--result_dir', required=True, help='Output directory for im
 # Core Options: these determine the shape/size of the neural network
 parser.add_argument('--dataset', help='Input filename (must be in .dataset format)')
 parser.add_argument('--example_count', type=int, default=100, help='Number of labels to train with')
-parser.add_argument('--epochs', type=int, default=25, help='Max number of training epochs [default: 50]')
+parser.add_argument('--classifier_epochs', type=int, default=25, help='Max number of training epochs [default: 50]')
 
 options = vars(parser.parse_args())
 
@@ -19,7 +19,7 @@ options = vars(parser.parse_args())
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from dataloader import CustomDataloader
 from training import train_classifier
-from evaluation import evaluate_classifier
+from evaluation import evaluate_classifier, save_evaluation
 from networks import build_networks, get_optimizers
 from options import save_options, load_options, get_current_epoch
 from locking import acquire_lock, release_lock
@@ -31,6 +31,7 @@ loaded_options = load_options(options)
 train_dataloader = CustomDataloader(fold='train', **options)
 
 # A larger test dataset for evaluation
+# TODO: This should be the validation fold, or a hold-out set from fold='train'
 test_dataloader_options = options.copy()
 del test_dataloader_options['example_count']
 test_dataloader = CustomDataloader(fold='test', **test_dataloader_options)
@@ -41,21 +42,22 @@ optimizers = get_optimizers(networks, **options)
 acquire_lock(options['result_dir'])
 try:
     test_accuracy = 0
-    for epoch in range(options['epochs']):
+    for classifier_epoch in range(options['classifier_epochs']):
         # Apply learning rate decay
         for optimizer in optimizers.values():
-            optimizer.param_groups[0]['lr'] = options['lr'] * (options['decay'] ** epoch)
+            optimizer.param_groups[0]['lr'] = options['lr'] * (options['decay'] ** classifier_epoch)
         # Train for one epoch
-        train_classifier(networks, optimizers, train_dataloader, epoch=epoch, **options)
-        # Evaluate
-        options['fold'] = 'semisupervised_{}_example_count_{:06d}'.format(options['dataset'], options['example_count'])
-        evals = evaluate_classifier(networks, test_dataloader, verbose=False, **options)
-        acc = list(evals.values())[0]['accuracy'] 
-        print("Accuracy with {} labels: {:.3f}".format(options['example_count'], acc))
-        if acc < test_accuracy:
-            print("Overfit at {} epochs, quitting".format(epoch))
-            break
-        else:
-            test_accuracy = acc
+        train_classifier(networks, optimizers, train_dataloader, epoch=classifier_epoch, **options)
+
+    # Evaluate
+    foldname = 'semisupervised_{}_example_count_{:06d}'.format(test_dataloader.dsf.name, options['example_count'])
+    classifier_options = options.copy()
+    classifier_options['fold'] = foldname
+
+    new_results = evaluate_classifier(networks, test_dataloader, verbose=False, **classifier_options)
+    acc = list(new_results.values())[0]['accuracy']
+
+    print("Test set accuracy with {} labels: {:.3f}".format(options['example_count'], acc))
+    save_evaluation(new_results, options['result_dir'], get_current_epoch(options['result_dir']))
 finally:
     release_lock(options['result_dir'])
