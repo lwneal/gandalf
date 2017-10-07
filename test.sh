b7ee75732874df1df668ada0c876f5e9c345cb88
Larry Neal <nealla@lwneal.com>
2017-10-06 22:32:05 -0700

    Add test script
    Just ensures that the data and results directories exist, and then
    runs a simple MNIST test

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# TODO: Make sure everything is installed correctly
+
+function make_sure_directory_exists() {
+    if [ ! -d $1 ]; then
+        echo "Directory $1 does not exist, creating it now"
+        mkdir -p $1
+    else
+        echo "Directory $1 exists"
+    fi
+}
+
+make_sure_directory_exists /mnt/results
+make_sure_directory_exists /mnt/data
+
+python datasets/download_mnist.py
+
+experiments/train_aac.py \
+    --result_dir /mnt/results/mnist_28x28 \
+    --dataset /mnt/data/mnist.dataset \
+    --image_size 28 \
+    --encoder encoder28 \
+    --generator generator28 \
+    --discriminator discriminator28 \
+    --epochs 1
