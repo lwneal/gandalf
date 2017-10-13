d42d93ad6dce660476dfbb6213e3d83a2ef45965
Larry Neal <nealla@lwneal.com>
2017-10-12 20:58:30 -0700

    Add 64x spherical encoder

--- network_definitions.py
+++ network_definitions.py
@@ -683,6 +683,44 @@ class encoderLReLU64(nn.Module):
 encoder64 = encoderLReLU64
 
 
+class encoder64sphere(nn.Module):
+    def __init__(self, latent_size=100, **kwargs):
+        super(self.__class__, self).__init__()
+        self.latent_size = latent_size
+        self.conv1 = nn.Conv2d(3,       64,     4, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(64,      128,    4, 2, 1, bias=False)
+        self.conv3 = nn.Conv2d(128,     256,    4, 2, 1, bias=False)
+        self.conv4 = nn.Conv2d(256,     512,    4, 2, 1, bias=False)
+        self.conv5 = nn.Conv2d(512,     1024,   4, 1, 0, bias=False)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.bn3 = nn.BatchNorm2d(512)
+        self.fc1 = nn.Linear(1024, self.latent_size, bias=False)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv4(x)
+        x = self.bn3(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv5(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = x.squeeze()
+        x = self.fc1(x)
+        xnorm = xnorm.expand(1, x.size()[0])
+        xnorm = xnorm.transpose(1,0)
+        x = x.div(xnorm)
+        return x
+
+
 class encoderLReLU128(nn.Module):
     def __init__(self, latent_size=100, **kwargs):
         super(self.__class__, self).__init__()
