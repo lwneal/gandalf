bb7a82c65dde0a9a1bc4493adbceb039b0f1ec5e
Larry Neal <nealla@lwneal.com>
2017-10-14 12:11:08 -0700

    Implement attribute counterfactuals
    This is the real point of the counterfactual optimization: we can
    take an example and change any one of several properties of it.
    
    Eg. faces can change hair, glasses, shape, features

--- counterfactual.py
+++ counterfactual.py
@@ -17,6 +17,7 @@ def generate_trajectory_batch(networks, dataloader, desired_class=None, **option
     netE = networks['encoder']
     netC = networks['classifier']
     netD = networks['discriminator']
+    netA = networks.get('attribute')
     result_dir = options['result_dir']
     batch_size = options['batch_size']
     image_size = options['image_size']
@@ -27,13 +28,15 @@ def generate_trajectory_batch(networks, dataloader, desired_class=None, **option
     desired_class_name = dataloader.lab_conv.labels[desired_class]
     print("Morphing input examples to class {}".format(desired_class_name))
 
-    real_images, labels, attributes = dataloader.get_batch()
+    real_images, labels, attributes = dataloader.get_batch(required_class=options['start_class'])
     real_images = Variable(real_images)
 
     # We start with vectors in the latent space Z
     z = to_np(netE(real_images))
     z = Variable(torch.FloatTensor(z), requires_grad=True).cuda()
+    original_z = z.clone()
     D_real = netD(real_images).data.cpu().numpy().mean()
+    original_preds = netA(z)
 
     # We want to move them so their classification changes
     target_labels = torch.LongTensor(batch_size)
@@ -63,29 +66,56 @@ def generate_trajectory_batch(networks, dataloader, desired_class=None, **option
     z_trajectory = []
 
     # TODO: Refactor this optimization into a function
-    for i in range(200):
-        hallucinations = netG(z)
-        cf_loss = nll_loss(netC(z), target_labels)
-        dc_dz = autograd.grad(cf_loss, z, cf_loss, retain_graph=True)[0]
-        momentum -= dc_dz * .01
-        z += momentum
-        momentum *= .99
+    for i in range(90):
+        target_attr = None
+        if options['zero_attribute']:
+            target_attr = options['zero_attribute']
+            target_attr_value = 0
+        if options['one_attribute']:
+            target_attr = options['one_attribute']
+            target_attr_value = 1
+
+        # Inner loop: Take several steps of gradient descent
+        for _ in range(10):
+            if target_attr:
+                attr_idx = dataloader.attr_conv.idx[target_attr]
+                preds = netA(z)
+                target_attrs = preds.clone()
+                target_attrs[:,attr_idx] = 0
+                yhat = preds[:,attr_idx]
+                epsilon = .01
+                cf_loss = -target_attr_value * torch.log(epsilon + yhat) - (1 - target_attr_value) * torch.log(epsilon + 1 - yhat)
+                cf_loss = torch.mean(cf_loss)
+            else:
+                cf_loss = nll_loss(netC(z), target_labels)
+
+            # Distance in latent space from original point
+            cf_loss += .002 * torch.sum((z - original_z) ** 2)
+
+            dc_dz = autograd.grad(cf_loss, z, cf_loss, retain_graph=True)[0]
+            momentum -= dc_dz * .01
+            z += momentum
+            momentum *= .5
+
         z_trajectory.append(to_np(z))
+
         print("Loss: {}".format(cf_loss.data[0]))
         print("Latent point: {}...".format(z[0].data.cpu().numpy()[:5]))
         print("Gradient: {}...".format(dc_dz[0].data.cpu().numpy()[:5]))
         print("Momentum: {}...".format(momentum[0].data.cpu().numpy()[:5]))
+
         classes = to_np(netC(z).max(1)[1])
         print("Class: {}...".format(classes))
 
+        hallucinations = netG(z)
         D_halluc = netD(hallucinations).data.cpu().numpy().mean()
-
+        
         caption = "DR {:.04f}  DG {:.04f}".format(D_real, D_halluc)
         imutil.show(hallucinations,
                 video_filename=video_filename,
                 caption=caption,
                 font_size=12,
-                display=False)
+                display=i % 25 == 0)
 
     # A .mp4 video for normal visualization
     imutil.encode_video(video_filename)
@@ -96,6 +126,14 @@ def generate_trajectory_batch(networks, dataloader, desired_class=None, **option
     trajectory_filename = video_filename.replace('.mjpeg', '.npy')
     print("Saving trajectory {}".format(trajectory_filename))
     np.save(trajectory_filename, np.array(z_trajectory))
+    
+    if netA:
+        print("Original Attributes:")
+        print(dataloader.attr_conv.attributes)
+        before = torch.sum(original_preds, 0)
+        print("Counterfactual Attributes:")
+        after = torch.sum(netA(z), 0)
+        print(after - before)
 
     return to_np(z)
 
--- experiments/generate_counterfactual.py
+++ experiments/generate_counterfactual.py
@@ -11,7 +11,10 @@ parser.add_argument('--result_dir', required=True, help='Output directory for im
 # Other options can change with every run
 parser.add_argument('--batch_size', type=int, default=64, help='Batch size [default: 64]')
 parser.add_argument('--fold', type=str, default='test', help='Fold [default: test]')
+parser.add_argument('--start_class', type=str, help='Start from one class instead of random sampling')
 parser.add_argument('--desired_class', type=int, help='Desired class number')
+parser.add_argument('--zero_attribute', type=str, help='Attribute to set to one')
+parser.add_argument('--one_attribute', type=str, help='Attribute to set to zero')
 parser.add_argument('--mode', default="batch", help='One of: batch, active [default: batch]')
 
 options = vars(parser.parse_args())
@@ -31,10 +34,10 @@ if options['mode'] == 'active':
     options['batch_size'] = 1
 else:
     # Batch Labeling trajectories must be 4x4 grids
-    options['batch_size'] = 16
+    options['batch_size'] = 64
 
 dataloader = CustomDataloader(**options)
-networks = build_networks(dataloader.num_classes, **options)
+networks = build_networks(dataloader.num_classes, dataloader.num_attributes, **options)
 
 start_epoch = get_current_epoch(options['result_dir'])
 print("Loaded model at epoch {}".format(start_epoch))
