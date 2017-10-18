3cee5bc1a80b5f4eeec444f8fbf8377ea0bdc423
Larry Neal <nealla@lwneal.com>
2017-10-18 12:33:04 -0700

    Add script to print out metrics
    Use it like:
    
        view_metrics train discriminator_mean
    
    to view the discriminator_mean value for the training fold for all experiments
    Outputs test accuracy by default.

---
+++ view_metrics.py
@@ -0,0 +1,87 @@
+# Print test_accuracy for each result directory
+import os
+import sys
+import json
+RESULTS_DIR = '/mnt/results'
+
+
+def get_result_dirs():
+    return [f for f in os.listdir(RESULTS_DIR) if os.path.isdir(os.path.join(RESULTS_DIR, f))]
+
+
+def get_results(result_dir, epoch):
+    filename = 'eval_epoch_{:04d}.json'.format(epoch)
+    filename = os.path.join(RESULTS_DIR, result_dir, filename)
+    if os.path.exists(filename):
+        eval_folds = json.load(open(filename))
+        return eval_folds
+    return {}
+
+
+def get_params(result_dir):
+    filename = 'params.json'
+    filename = os.path.join(RESULTS_DIR, result_dir, filename)
+    return json.load(open(filename))
+
+
+def get_dataset_name(result_dir):
+    params = get_params(result_dir)
+    dataset = params['dataset']
+    return dataset.split('/')[-1].replace('.dataset', '')
+
+
+def epoch_from_filename(filename):
+    numbers = filename.split('epoch_')[-1].rstrip('.pth')
+    return int(numbers)
+
+
+def is_valid_directory(result_dir):
+    result_dir = os.path.join(RESULTS_DIR, result_dir)
+    if not os.path.exists(result_dir) or not os.path.isdir(result_dir):
+        return False
+    if 'params.json' not in os.listdir(result_dir):
+        return False
+    dirs = os.listdir(result_dir)
+    if 'robotno' in dirs or 'norobot' in dirs:
+        print("Found robotno in {}, skipping".format(result_dir))
+        return False
+    return True
+
+
+def get_epochs(result_dir):
+    filenames = os.listdir(os.path.join(RESULTS_DIR, result_dir))
+    pth_names = [f for f in filenames if f.endswith('.pth')]
+    return sorted(list(set(epoch_from_filename(f) for f in pth_names)))
+
+
+def get_all_info(fold, metric):
+    info = []
+    for result_dir in get_result_dirs():
+        # Evaluate the most recent epoch
+        epochs = get_epochs(result_dir)
+        if not epochs:
+            continue
+        epoch = epochs[-1]
+        results = get_results(result_dir, epoch)
+        if not results:
+            continue
+        if fold not in results:
+            continue
+        info.append((result_dir, epoch, results[fold][metric]))
+    return info
+
+
+if __name__ == '__main__':
+    fold = 'test'
+    metric = 'accuracy'
+    if len(sys.argv) > 1:
+        fold = sys.argv[1]
+    if len(sys.argv) > 2:
+        metric = sys.argv[2]
+
+    infos = get_all_info(fold=fold, metric=metric)
+    infos.sort(key=lambda x: x[0].split('_')[0] + str(x[2]))
+
+    print('{:<64} {:>8} {:>8}'.format("Experiment Name", "Epoch", fold + '_' + metric))
+    for (name, epoch, metric) in infos:
+        print('{:<64} {:>8} {:>8.4f}'.format(name, epoch, metric))
