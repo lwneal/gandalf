5c1bea5d74988d52937aad9c73ec0d38312463d1
Larry Neal <nealla@lwneal.com>
2017-10-23 14:06:52 -0700

    Update SVHN dataset downloader

--- datasets/download_SVHN.py
+++ datasets/download_SVHN.py
@@ -5,12 +5,17 @@ import numpy as np
 import json
 from subprocess import check_output
 from scipy import io as sio
+from tqdm import tqdm
 
+from PIL import Image
 import matplotlib
 matplotlib.use('Agg')
 
-import matplotlib.pyplot as plt
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from imutil import show
 
 DOWNLOAD_URL_TRAIN = 'http://ufldl.stanford.edu/housenumbers/train.tar.gz'
 DOWNLOAD_URL_TEST = 'http://ufldl.stanford.edu/housenumbers/test.tar.gz'
@@ -18,12 +23,28 @@ DOWNLOAD_URL_TEST = 'http://ufldl.stanford.edu/housenumbers/test.tar.gz'
 DOWNLOAD_URL_TRAIN_MAT = 'http://ufldl.stanford.edu/housenumbers/train_32x32.mat'
 DOWNLOAD_URL_TEST_MAT = 'http://ufldl.stanford.edu/housenumbers/test_32x32.mat'
 
-#PARTITION_URL =  'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADxLE5t6HqyD8sQCmzWJRcHa/Eval/list_eval_partition.txt?dl=1'
-#ANNO_URL = 'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AAC7-uCaJkmPmvLX2_P5qy0ga/Anno/list_attr_celeba.txt?dl=1'
 
 DATA_DIR = '/mnt/data'
 DATASET_NAME = 'SVHN'
 DATASET_PATH = os.path.join(DATA_DIR, DATASET_NAME)
+
+def from_mat(mat, img_dir, fold):
+    data  = mat['X']
+    labels = mat['y']
+    examples = []
+    for i in tqdm(range(len(labels))):
+        img = data[:,:,:,i]
+        label = labels[i][0]
+        if label == 10:
+            label = 0
+        filename = os.path.join(img_dir, "{}_{:06d}.jpg".format(fold, i))
+        Image.fromarray(img).save(filename)
+        examples.append({
+            'fold': fold,
+            'label': int(label),
+            'filename': filename,
+        })
+    return examples
 
 
 def main():
@@ -36,19 +57,21 @@ def main():
 
     #download('train.tar.gz', DOWNLOAD_URL_TRAIN)
     #download('test.tar.gz', DOWNLOAD_URL_TEST)
-    #download('train_32x32.mat', DOWNLOAD_URL_TRAIN_MAT)
-    #download('test_32x32.mat', DOWNLOAD_URL_TEST_MAT)
+    download('train_32x32.mat', DOWNLOAD_URL_TRAIN_MAT)
+    download('test_32x32.mat', DOWNLOAD_URL_TEST_MAT)
 
-    #copied from pytorch dataset svhn code
-    os.chdir(DATASET_PATH + '/train')
-    loaded_mat = sio.loadmat('train_32x32.mat')
+    
+    #os.chdir(DATASET_PATH + '/train')
+    train_mat = sio.loadmat('train_32x32.mat')
+    test_mat = sio.loadmat('test_32x32.mat')
 
-    data = loaded_mat['X']
+    test_data = test_mat['X']
+    test_labels = test_mat['y']
+
     # loading from the .mat file gives an np array of type np.uint8
     # converting to np.int64, so that we have a LongTensor after
     # the conversion from the numpy array
     # the squeeze is needed to obtain a 1D tensor
-    labels = loaded_mat['y']
 
     # the svhn dataset assigns the class label "10" to the digit 0
     # this makes it inconsistent with several loss functions
@@ -56,33 +79,13 @@ def main():
     #np.place(labels, labels == 10, 0)
     #data = np.transpose(data, (3, 2, 0, 1))
 
-    #https://stackoverflow.com/questions/29185493/read-svhn-dataset-in-python
-    image_ind = 10
-    #print(data[image_ind])
-    plt.imshow(data[:,:,:,image_ind])
-    #plt.imsave('test.png',data[image_ind], cmap=plt.cm.gray)
-    print (labels[image_ind])
-    exit()
-    imagelabels_mat = sio.loadmat('digitStruct.mat')
-
-
-
-
-    images = listdir(os.path.join(DATASET_PATH, 'img_align_SVHN'))
-
-    # Read training/test split
-    is_training = train_test_split('list_eval_partition.txt')
-    assert len(is_training) == len(images)
-
-    examples = []
-    raw_attrs = load_attributes('list_attr_celeba.txt')
-    for attr, is_training in zip(raw_attrs, is_training):
-        example = {'is_' + k.lower(): v for (k, v) in attr.items()}
-        example['fold'] = 'train' if is_training else 'test'
-        examples.append(example)
+    IMG_DIR = os.path.join(DATASET_PATH, 'images')
+    if not os.path.exists(IMG_DIR):
+        os.mkdir(IMG_DIR)
+    examples = from_mat(train_mat, IMG_DIR, 'train') + from_mat(test_mat, IMG_DIR, 'test')
 
     # Generate CSV file for the full dataset
-    save_image_dataset(images, examples)
+    save_examples(examples)
     print("Successfully built dataset {}".format(DATASET_PATH))
 
 
@@ -111,37 +114,17 @@ def download(filename, url):
         elif filename.endswith('.zip'):
             os.system('unzip *.zip')
         elif filename.endswith('.mat'):
-            os.system('mv ' + filename + ' ' + DATASET_PATH + '/' + mv_dir)
+            os.system('cp ' + filename + ' ' + DATASET_PATH + '/' + mv_dir)
 
 
-def train_test_split(filename):
-    # Training examples end with 0, test with 1, validation with 2
-    return [line.strip().endswith('0') for line in open(filename)]
 
-
-def load_attributes(filename):
-    attrs = []
-    lines = open(filename).readlines()
-    columns = lines[1].split()
-    for line in lines[2:]:
-        attr = {}
-        booleans = [word == '1' for word in line.split()[1:]]
-        for attr_name, attr_val in zip(columns, booleans):
-            attr[attr_name] = attr_val
-        attrs.append(attr)
-    return attrs
-
-
-def save_image_dataset(images, attributes):
+def save_examples(examples):
     output_filename = '{}/{}.dataset'.format(DATA_DIR, DATASET_NAME)
-    print("Writing {} items to {}".format(len(images), output_filename))
+    print("Writing {} items to {}".format(len(examples), output_filename))
 
     fp = open(output_filename, 'w')
-    for filename, attrs in zip(images, attributes):
-        line = attrs
-        line['filename'] = filename
-        line['label'] = attrs['is_male']
-        fp.write(json.dumps(line) + '\n')
+    for ex in examples:
+        fp.write(json.dumps(ex) + '\n')
     fp.close()
 
 
