e176b9cafdc1da0518233b53e6db6a3b01a9b295
Larry Neal <nealla@lwneal.com>
2017-10-23 12:23:18 -0700

    Fix null classifier_name

--- experiments/generate_counterfactual.py
+++ experiments/generate_counterfactual.py
@@ -16,7 +16,8 @@ parser.add_argument('--desired_class', type=int, help='Desired class number')
 parser.add_argument('--zero_attribute', type=str, help='Attribute to set to one')
 parser.add_argument('--one_attribute', type=str, help='Attribute to set to zero')
 parser.add_argument('--mode', default="batch", help='One of: batch, active, uncertainty [default: batch]')
-parser.add_argument('--classifier_name', type=str, help='Name of the classifier to use')
+parser.add_argument('--classifier_name', type=str, default='active_learning_classifier',
+        help='Name of the classifier to use [default: active_learning_classifier]')
 
 options = vars(parser.parse_args())
 
