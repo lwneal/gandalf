67b7e0ef6f85ecdae959b74f06150ad2b66173b2
Larry Neal <nealla@lwneal.com>
2017-10-27 19:51:31 -0700

    Remove everything except active counterfactuals

--- experiments/generate_counterfactual.py
+++ experiments/generate_counterfactual.py
@@ -16,59 +16,31 @@ parser.add_argument('--batch_size', type=int, default=64, help='Batch size [defa
 parser.add_argument('--fold', type=str, default='test', help='Fold [default: test]')
 parser.add_argument('--start_class', type=str, help='Name of starting class (random by default)')
 parser.add_argument('--target_class', type=str, help='Name target class (random by default)')
-parser.add_argument('--zero_attribute', type=str, help='Attribute to set to one')
-parser.add_argument('--one_attribute', type=str, help='Attribute to set to zero')
-parser.add_argument('--mode', default="batch", help='One of: batch, random, uncertainty [default: batch]')
-parser.add_argument('--counterfactual_frame_count', default=60, type=int,
-        help='Number of frames to output [default: 60]')
+parser.add_argument('--counterfactual_frame_count', default=100, type=int,
+        help='Number of frames to output [default: 100]')
 parser.add_argument('--classifier_name', type=str, default='active_learning_classifier',
         help='Name of the classifier to use [default: active_learning_classifier]')
 parser.add_argument('--speed', type=float, default=.001, help='Learning rate for counterfactual descent [default: .001]')
 parser.add_argument('--momentum_mu', type=float, default=.95, help='Momentum decay (zero for no momentum) [default: .95]')
 parser.add_argument('--counterfactual_max_iters', type=int, default=1000, help='Maximum number of steps to take for CF trajectories [default: 1000]')
-parser.add_argument('--write_jpgs', type=is_true, default=False, help='Write extra JPG files for visualization')
 parser.add_argument('--start_epoch', type=int, help='Epoch to start from (defaults to most recent epoch)')
 
 options = vars(parser.parse_args())
 
-
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from dataloader import CustomDataloader
 import counterfactual
-from networks import build_networks, get_optimizers
-from options import load_options, get_current_epoch
+from networks import build_networks
+from options import load_options
 
 
 options = load_options(options)
 
-if options['mode'] == 'batch':
-    # Batch Labeling trajectories must be 4x4 grids
-    options['batch_size'] = 64
-else:
-    # Active Learning trajectories can only be single examples
-    options['batch_size'] = 1
+# Active Learning trajectories can only be single examples
+# TODO: enable batches
+options['batch_size'] = 1
 
 dataloader = CustomDataloader(**options)
-
 networks = build_networks(dataloader.num_classes, dataloader.num_attributes, **options)
 
-start_epoch = options['start_epoch']
-if start_epoch is None:
-    start_epoch = get_current_epoch(options['result_dir'])
-print("Loaded model at epoch {}".format(start_epoch))
-
-if options['mode'] == 'batch':
-    # This one is a good visualization and can be used for efficient labeling
-    counterfactual.generate_trajectory_batch(networks, dataloader, **options)
-elif options['mode'] == 'random':
-    # This is the classic visualization for a single example
-    counterfactual.generate_trajectory_active(networks, dataloader, **options)
-elif options['mode'] == 'uncertainty':
-    # Uncertainty Sampling used for Experiment 1. Requires --classifier_name
-    counterfactual.generate_trajectory_active(networks, dataloader, strategy='uncertainty-top2', **options)
-elif options['mode'] == 'uncertainty-top1':
-    # Counterfactual strategy: start at an uncertain point, make it more certain
-    counterfactual.generate_trajectory_active(networks, dataloader, strategy='uncertainty-top1', **options)
-elif options['mode'] == 'random-top1':
-    # Counterfactual strategy: start at a random point, move to a known class
-    counterfactual.generate_trajectory_active(networks, dataloader, strategy='random-top1', **options)
+counterfactual.generate_trajectory_active(networks, dataloader, **options)
