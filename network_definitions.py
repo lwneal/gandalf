e3bcb409594b0f86ba6e7c6677c4eb9f086f1f6d
Larry Neal <nealla@lwneal.com>
2017-10-31 00:23:02 -0700

    More networks (especially instance norm)

--- network_definitions.py
+++ network_definitions.py
@@ -590,6 +590,38 @@ class discriminatorLReLU64(nn.Module):
 discriminator64 = discriminatorLReLU64
 
 
+class discriminator64instancenorm(nn.Module):
+    def __init__(self, latent_size=100, **kwargs):
+        super(self.__class__, self).__init__()
+        self.conv1 = nn.Conv2d(3,       64,      4, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(64,      128,     4, 2, 1, bias=False)
+        self.conv3 = nn.Conv2d(128,     256,     4, 2, 1, bias=False)
+        self.conv4 = nn.Conv2d(256,     512,     4, 2, 1, bias=False)
+        self.conv5 = nn.Conv2d(512,     1,       4, 1, 0, bias=False)
+        self.bn1 = nn.InstanceNorm2d(128)
+        self.bn2 = nn.InstanceNorm2d(256)
+        self.bn3 = nn.InstanceNorm2d(512)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv4(x)
+        x = self.bn3(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv5(x)
+        # Global average pooling
+        x = x.mean(-1).mean(-1)
+        return x.view(-1, 1).squeeze(1)
+
+
 class discriminator64log(nn.Module):
     def __init__(self, latent_size=100, **kwargs):
         super(self.__class__, self).__init__()
