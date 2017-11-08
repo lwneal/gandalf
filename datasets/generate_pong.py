adf8a425d7d881c163b24b5d4ab0e8751fe8379e
Larry Neal <nealla@lwneal.com>
2017-11-08 13:05:50 -0800

    New Pong image generator

--- datasets/generate_pong.py
+++ datasets/generate_pong.py
@@ -1,30 +1,67 @@
 # A script to save Pong images to file
 # Requires https://github.com/openai/gym#atari
 
+from tqdm import tqdm
+import numpy as np
 import os
 import json
 from PIL import Image
 import gym
 
 ITERS = 50000
+BACKGROUND_COLOR = [144,72,17]
+PADDLE_COLOR = 213
+LEFT_PADDLE_COL = 9
+DATASET_NAME = 'pong-random'
+DIR_NAME = os.path.join('/mnt/data', DATASET_NAME)
+np.random.seed(42)
 
-if not os.path.exists('/mnt/data/pong'):
-    os.mkdir('/mnt/data/pong')
-fp = open('/mnt/data/pong.dataset', 'w')
 
-env = gym.make('Pong-v0')
-obs = env.reset()
-for i in range(10 * ITERS):
+def get_left_paddle_height(state):
+    left_paddle_col = state[:,9,0]
+    left_paddle_row = np.argwhere(left_paddle_col == PADDLE_COLOR)
+    if len(left_paddle_row) == 0:
+        return None
+    return left_paddle_row[0,0]
+
+def get_label_for_state(state):
+    height = get_left_paddle_height(state)
+    if height is None:
+        return None
+    return 'Bottom' if height >= 36 else 'Top'
+
+def step(env):
     img, reward, game_over, foo = env.step(env.action_space.sample())
     if game_over:
         env.reset()
-    state = img[34:194:2,::2]  # The 160x160 square with the paddles, subsampled 2x2
-    if i % 10 == 0:
-        filename = '/mnt/data/pong/{:06d}.png'.format(i)
-        Image.fromarray(state).save(filename)
-        fold = 'test' if i % 100 == 0 else 'train'
-        label = 'Bad' if reward < 0 else 'Good'
-        example = {'filename': filename, 'label': reward, 'fold': fold}
-        fp.write(json.dumps(example) + '\n')
-fp.close()
+    # The 160x160 square with the paddles, subsampled 2x2
+    play_field_state = img[34:194:2,::2]  
+    return play_field_state
 
+
+def main():
+    if not os.path.exists(DIR_NAME):
+        os.mkdir(DIR_NAME)
+
+    fp = open('/mnt/data/{}.dataset'.format(DATASET_NAME), 'w')
+    env = gym.make('Pong-v0')
+    obs = env.reset()
+    for i in tqdm(range(10 * ITERS)):
+        state = step(env)
+        label = get_label_for_state(state)
+        if label is None:
+            continue
+        if i % 10 == 0:
+            filename = os.path.join(DIR_NAME, '{:06d}.png'.format(i))
+            Image.fromarray(state).save(filename)
+            fold = 'test' if i % 100 == 0 else 'train'
+            example = {
+                'filename': filename,
+                'label': label,
+                'fold': fold,
+            }
+            fp.write(json.dumps(example) + '\n')
+    fp.close()
+
+if __name__ == '__main__':
+    main()
