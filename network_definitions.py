3ecaf1b9ec603cc631aca09eed5fa6a65112c994
Larry Neal <nealla@lwneal.com>
2017-11-09 10:59:33 -0800

    Add new default 128x128 networks

--- network_definitions.py
+++ network_definitions.py
@@ -471,7 +471,45 @@ class generatorLeakyReLU128(nn.Module):
         x = self.conv6(x)
         x = nn.Sigmoid()(x)
         return x
-generator128 = generatorLeakyReLU128
+
+class generator128(nn.Module):
+    def __init__(self, latent_size=100, **kwargs):
+        super(self.__class__, self).__init__()
+        Z = latent_size
+        self.conv1 = nn.ConvTranspose2d(     Z,   1024, 4, 1, 0, bias=False)
+        self.conv2 = nn.ConvTranspose2d(  1024,    1024, 4, 2, 1, bias=False)
+        self.conv3 = nn.ConvTranspose2d(   1024,    512, 4, 2, 1, bias=False)
+        self.conv4 = nn.ConvTranspose2d(   512,    256, 4, 2, 1, bias=False)
+        self.conv5 = nn.ConvTranspose2d(   256,     128, 4, 2, 1, bias=False)
+        self.conv6 = nn.ConvTranspose2d(    128,      3, 4, 2, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(1024)
+        self.bn2 = nn.BatchNorm2d(1024)
+        self.bn3 = nn.BatchNorm2d(512)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = x.unsqueeze(-1).unsqueeze(-1)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(True)(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(True)(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = nn.LeakyReLU(True)(x)
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = nn.LeakyReLU(True)(x)
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = nn.LeakyReLU(True)(x)
+        x = self.conv6(x)
+        x = nn.Sigmoid()(x)
+        return x
 
 
 class discriminator32(nn.Module):
@@ -755,7 +793,6 @@ class discriminatorMultiscale128(nn.Module):
 
         wasserstein_distance = critic1 + critic2 + critic3
         return wasserstein_distance.squeeze()
-discriminator128 = discriminatorMultiscale128
 
 
 class discriminator128instancenorm(nn.Module):
@@ -806,6 +843,42 @@ class discriminator128instancenorm(nn.Module):
 
         wasserstein_distance = critic1 + critic2 + critic3
         return wasserstein_distance.squeeze()
+
+
+class discriminator128(nn.Module):
+    def __init__(self, latent_size=100, **kwargs):
+        super(self.__class__, self).__init__()
+        self.conv1 = nn.Conv2d(3,       128,      4, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(128,      256,     4, 2, 1, bias=False)
+        self.conv3 = nn.Conv2d(256,     512,     4, 2, 1, bias=False)
+        self.conv4 = nn.Conv2d(512,     1024,     4, 2, 1, bias=False)
+        self.conv5 = nn.Conv2d(1024,     1024,    4, 2, 1, bias=False)
+        self.conv6 = nn.Conv2d(1024,    1,       4, 1, 0, bias=False)
+        self.bn1 = nn.InstanceNorm2d(256)
+        self.bn2 = nn.InstanceNorm2d(512)
+        self.bn3 = nn.InstanceNorm2d(1024)
+        self.bn4 = nn.InstanceNorm2d(1024)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv4(x)
+        x = self.bn3(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv5(x)
+        x = self.bn4(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv6(x)
+        score = x.mean(-1).mean(-1)
+        return score.squeeze()
 
 
 class encoder28(nn.Module):
@@ -1160,11 +1233,54 @@ class encoderLReLU128(nn.Module):
         x = x.squeeze(-1).squeeze(-1)
         x = self.fc1(x)
         return x
-encoder128 = encoderLReLU128
 
 
 
 class encoder128sphere(nn.Module):
+    def __init__(self, latent_size=100, **kwargs):
+        super(self.__class__, self).__init__()
+        self.latent_size = latent_size
+        # in_channels, out_channels, kernel_size, stride, padding, dilation
+        self.conv1 = nn.Conv2d(3,       64,     4, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(64,      128,    4, 2, 1, bias=False)
+        self.conv3 = nn.Conv2d(128,     256,    4, 2, 1, bias=False)
+        self.conv4 = nn.Conv2d(256,     512,    4, 2, 1, bias=False)
+        self.conv5 = nn.Conv2d(512,     1024,   4, 2, 1, bias=False)
+        self.conv6 = nn.Conv2d(1024,    2048,   4, 1, 0, bias=False)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.bn3 = nn.BatchNorm2d(512)
+        self.bn4 = nn.BatchNorm2d(1024)
+        self.fc1 = nn.Linear(2048, self.latent_size, bias=False)
+        self.apply(weights_init)
+        self.cuda()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv4(x)
+        x = self.bn3(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv5(x)
+        x = self.bn4(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = self.conv6(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
+        x = x.squeeze(-1).squeeze(-1)
+        x = self.fc1(x)
+        xnorm = torch.norm(x, p=2, dim=1).detach()
+        xnorm = xnorm.expand(1, x.size()[0])
+        xnorm = xnorm.transpose(1,0)
+        x = x.div(xnorm)
+        return x
+
+class encoder128(nn.Module):
     def __init__(self, latent_size=100, **kwargs):
         super(self.__class__, self).__init__()
         self.latent_size = latent_size
