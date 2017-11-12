ffeaca05db8b558eb1f1027634f9e9fdda1c7a67
Larry Neal <nealla@lwneal.com>
2017-11-11 22:29:18 -0800

    Better title

--- plot_vs.py
+++ plot_vs.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     names = sys.argv[1:]
     #title = "Open Set MNIST vs EMNIST: 1000 initial points"
     #plotting.compare_multiple(sys.argv[1:], names, 'output.png', title=title, prefix='openset')
-    title = "MNIST Accuracy: Human-Labeled Counterfactuals"
+    title = "MNIST Active Learning Accuracy with Human Labels"
     plotting.compare_multiple(sys.argv[1:], names, 'mnist-human-counterfactual.png', title=title)
