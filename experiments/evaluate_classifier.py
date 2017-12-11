cf013059890f82be8fc08eaa202c3f42a844ba3b
Larry Neal <nealla@lwneal.com>
2017-12-11 13:43:33 -0800

    Add TODO

--- experiments/evaluate_classifier.py
+++ experiments/evaluate_classifier.py
@@ -30,6 +30,7 @@ dataloader = CustomDataloader(last_batch=True, shuffle=False, **options)
 networks = build_networks(dataloader.num_classes, dataloader.num_attributes, **options)
 
 if options['comparison_dataset']:
+    # TODO: Fix differences between this and evaluate_classifier
     # Hack: Switch to a new dataset and make up a descriptive fold name
     options['dataset'] = options['comparison_dataset']
     dataloader = CustomDataloader(last_batch=True, shuffle=False, **options)
