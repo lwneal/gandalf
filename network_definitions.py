1d677d4ad68d72f7c25bd2447d771c044bac83e8
Larry Neal <nealla@lwneal.com>
2017-12-12 10:24:34 -0800

    Remove old +1 classifier
    The two-layer linear/softmax system works much better

--- network_definitions.py
+++ network_definitions.py
@@ -1339,19 +1339,3 @@ class classifierLinear(nn.Module):
         x = nn.LeakyReLU(0.2, inplace=True)(x)
         x = self.fc2(x)
         return x
-
-
-class classifierLinearPlusOne(nn.Module):
-    def __init__(self, latent_size=100, num_classes=2, **kwargs):
-        super(self.__class__, self).__init__()
-        self.latent_size = latent_size
-        self.fc1 = nn.Linear(latent_size, 256)
-        self.fc2 = nn.Linear(256, num_classes + 1)
-        self.apply(weights_init)
-        self.cuda()
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = nn.LeakyReLU(0.2, inplace=True)(x)
-        x = self.fc2(x)
-        return x
