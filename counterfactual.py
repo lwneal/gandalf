ddcb4dcfd639ed983627dd2b3906eb4c7dc660f9
Larry Neal <nealla@lwneal.com>
2017-12-20 23:55:34 -0800

    Update counterfactual generation for the new model

--- counterfactual.py
+++ counterfactual.py
@@ -11,113 +11,6 @@ import imutil
 from imutil import VideoMaker
 
 
-# Morphs an entire batch of input examples into a given target_class
-def generate_trajectory_batch(networks, dataloader, target_class=None, **options):
-    netG = networks['generator']
-    netE = networks['encoder']
-    netC = networks['classifier']
-    netD = networks['discriminator']
-    result_dir = options['result_dir']
-    batch_size = options['batch_size']
-    image_size = options['image_size']
-    latent_size = options['latent_size']
-    speed = options['speed']
-    momentum_mu = options['momentum_mu']
-    max_iters = options['counterfactual_max_iters']
-
-    if target_class is None:
-        target_class = random.randint(0, dataloader.num_classes - 1)
-    target_class_name = dataloader.lab_conv.labels[target_class]
-    print("Morphing input examples to class {}".format(target_class_name))
-
-    real_images, labels = dataloader.get_batch(required_class=options['start_class'])
-    real_images = Variable(real_images)
-
-    # We start with vectors in the latent space Z
-    z = to_np(netE(real_images))
-    z = Variable(torch.FloatTensor(z), requires_grad=True).cuda()
-    original_z = z.clone()
-    D_real = netD(real_images).data.cpu().numpy().mean()
-    if netA:
-        original_preds = netA(z)
-
-    # We want to move them so their classification changes
-    target_labels = torch.LongTensor(batch_size)
-    target_labels[:] = target_class
-    target_labels = Variable(target_labels).cuda()
-
-    momentum = Variable(torch.zeros(z.size())).cuda()
-
-    # Write all counterfactuals to the trajectories/ subdirectory
-    trajectory_id = '{}_{}'.format(dataloader.dsf.name, int(time.time() * 1000))
-    video_filename = 'batch-{}-{}.mjpeg'.format(trajectory_id, target_class_name)
-    video_filename = os.path.join('trajectories', video_filename)
-    video_filename = os.path.join(options['result_dir'], video_filename)
-
-    path = os.path.join(options['result_dir'], 'trajectories')
-    if not os.path.exists(path):
-        print("Creating trajectories directory {}".format(path))
-        os.mkdir(path)
-
-    # First frame: originals, for comparison
-    imutil.show(real_images,
-            video_filename=video_filename,
-            caption="Original",
-            font_size=12,
-            display=False)
-
-    z_trajectory = []
-
-    # TODO: Refactor all of this into a function, have it return the trajectory
-    # Then do processing on the trajectory
-    for i in range(90):
-
-        # Inner loop: Take several steps of gradient descent
-        for _ in range(10):
-            # Class-based counterfactual
-            cf_loss = nll_loss(netC(z), target_labels)
-
-            # Distance in latent space from original point
-            cf_loss += .00001 * torch.sum((z - original_z) ** 2)
-
-            dc_dz = autograd.grad(cf_loss, z, cf_loss, retain_graph=True)[0]
-            momentum -= dc_dz * speed
-            z += momentum
-            momentum *= momentum_mu
-
-        z_trajectory.append(to_np(z))
-
-        print("Loss: {}".format(cf_loss.data[0]))
-        print("Latent point: {}...".format(z[0].data.cpu().numpy()[:5]))
-        print("Gradient: {}...".format(dc_dz[0].data.cpu().numpy()[:5]))
-        print("Momentum: {}...".format(momentum[0].data.cpu().numpy()[:5]))
-
-        classes = to_np(netC(z).max(1)[1])
-        print("Class: {}...".format(classes))
-
-        hallucinations = netG(z)
-        D_halluc = netD(hallucinations).data.cpu().numpy().mean()
-        
-        caption = "DR {:.04f}  DG {:.04f}".format(D_real, D_halluc)
-        imutil.show(hallucinations,
-                video_filename=video_filename,
-                caption=caption,
-                font_size=12,
-                display=i % 25 == 0)
-
-    # A .mp4 video for normal visualization
-    imutil.encode_video(video_filename)
-
-    # A still frame with no caption, used for the Batch Labeling UI
-    imutil.show(hallucinations, filename=video_filename.replace('.mjpeg', '.jpg'))
-
-    trajectory_filename = video_filename.replace('.mjpeg', '.npy')
-    print("Saving trajectory {}".format(trajectory_filename))
-    np.save(trajectory_filename, np.array(z_trajectory))
-    
-    return to_np(z)
-
-
 def to_torch(z, requires_grad=False):
     return Variable(torch.FloatTensor(z), requires_grad=requires_grad).cuda()
 
@@ -126,156 +19,13 @@ def to_np(z):
     return z.data.cpu().numpy()
 
 
-def generate_trajectory_active(networks, dataloader, strategy='random', **options):
-    netG = networks['generator']
-    netE = networks['encoder']
-    netC = networks['classifier']
-    netD = networks['discriminator']
-    result_dir = options['result_dir']
-    image_size = options['image_size']
-    latent_size = options['latent_size']
-    output_frame_count = options['counterfactual_frame_count']
-    speed = options['speed']
-    momentum_mu = options['momentum_mu']
-    max_iters = options['counterfactual_max_iters']
-    result_dir = options['result_dir']
-
-    if strategy == 'random':
-        # Start with a random example, move it to a random class
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC, pool_size=1)
-        start_class = most_likely_class
-        target_class = random_target_class(dataloader, start_class)
-    elif strategy == 'random-nearest':
-        # Start with a random example, move it to the nearest decision boundary
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC, pool_size=1)
-        start_class = most_likely_class
-        target_class = most_likely_class
-    elif strategy == 'uncertainty-random':
-        # Start with an uncertain example, move it to a random class
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC)
-        start_class = most_likely_class
-        target_class = random_target_class(dataloader, start_class)
-    elif strategy == 'uncertainty-nearest':
-        # Start with an uncertain example, move it to the nearest class
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC)
-        start_class = most_likely_class
-        target_class = most_likely_class
-    elif strategy == 'certainty-random':
-        # Start with an easy-to-classify sample, move it to the least likely class
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC, reverse=True)
-        start_class = most_likely_class
-        target_class = random_target_class(dataloader, start_class)
-    elif strategy == 'certainty-furthest':
-        # Start with an easy-to-classify sample, move it to the least likely class
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC, reverse=True)
-        start_class = most_likely_class
-        target_class = least_likely_class
-    elif strategy == 'random-interpolate':
-        start_class, _, _, start_img = select_uncertain_example(dataloader, netE, netC, pool_size=1)
-        target_class, _, _, target_img = select_uncertain_example(dataloader, netE, netC, pool_size=1)
-    else:
-        raise ValueError("Unknown strategy")
-
-    if strategy == 'random-interpolate':
-        z_trajectory = [to_np(netE(Variable(start_img))), to_np(netE(Variable(target_img)))]
-    else:
-        # Generate a path in latent space from start_img to a known classification
-        z = netE(Variable(start_img))
-        z_trajectory = generate_z_trajectory(z, target_class, netC, dataloader, speed, momentum_mu, max_iters=max_iters)
-
-    sampled_trajectory = sample_trajectory(z_trajectory, output_frame_count)
-
-    # Save the trajectory in .npy to later load
-    video_filename = make_video_filename(result_dir, dataloader, start_class, target_class)
-    trajectory_filename = video_filename.replace('.mjpeg', '.npy')
-    print("Saving trajectory length {} to {}".format(len(sampled_trajectory), trajectory_filename))
-    np.save(trajectory_filename, np.array(sampled_trajectory))
-
-    # Write a video of the trajectory
-    vid = VideoMaker(video_filename)
-    for z in sampled_trajectory:
-        z = to_torch(z)
-        hallucination = netG(z)
-        preds = netC(z)
-        pred_conf, pred_idx = torch.exp(preds).max(1)
-        pred_conf, pred_idx = to_np(pred_conf)[0], to_np(pred_idx)[0]
-        caption = "Class: {} (confidence {:.3f})".format(dataloader.lab_conv.labels[pred_idx], pred_conf)
-        vid.write_frame(hallucination, caption)
-    vid.finish()
-
-
-def generate_comparison(networks, dataloader, **options):
-    for net in networks:
-        networks[net].eval()
-    netG = networks['generator']
-    netE = networks['encoder']
-    netC = networks['classifier']
-    netD = networks['discriminator']
-    result_dir = options['result_dir']
-    image_size = options['image_size']
-    latent_size = options['latent_size']
-    output_frame_count = options['counterfactual_frame_count']
-    speed = options['speed']
-    momentum_mu = options['momentum_mu']
-    max_iters = options['counterfactual_max_iters']
-    result_dir = options['result_dir']
-
-
-    # For MNIST, SVHN
-    N = dataloader.num_classes
-    counterfactual_latent_points = []
-    for _ in range(N):
-        # Start with a random example
-        most_likely_class, least_likely_class, start_score, start_img = select_uncertain_example(dataloader, netE, netC, pool_size=1)
-        print("Got an example, the classifier thinks it is class: {}".format(most_likely_class))
-        start_class = most_likely_class
-
-        for class_idx in range(N):
-            # Generate a path in latent space from start_img to a known classification
-            z = netE(Variable(start_img))
-            target_class = class_idx
-            z_trajectory = generate_z_trajectory(z, target_class, netC, netE, netG, dataloader, speed, momentum_mu, max_iters=max_iters)
-            print("Generating what the image would look like if it were class {}".format(target_class))
-            counterfactual_latent_points.append(z_trajectory[-1])
-
-    images = []
-    original_img = start_img.cpu().numpy().squeeze(0)
-    reencoded = to_np(netG(netE(Variable(start_img)))).squeeze(0)
-    #images.append(original_img)
-    #images.append(reencoded)
-    for z in counterfactual_latent_points:
-        img = to_np(netG(to_torch(z))).squeeze(0)
-        images.append(img)
-
-    images = np.array(images).transpose((0,2,3,1))
-    video_filename = make_video_filename(result_dir, dataloader, start_class, start_class, label_type='grid')
-
-    # Save the images in npy format to re-load as training data
-    trajectory_filename = video_filename.replace('.mjpeg', '.npy')
-    np.save(trajectory_filename, images)
-
-    # Save the images in jpg format to display to the user
-    imutil.show(images, filename=video_filename.replace('.mjpeg', '.jpg'))
-
-
+# Just picks ground-truth examples with the right labels
 def generate_grid(networks, dataloader, **options):
-    netG = networks['generator']
-    netE = networks['encoder']
-    netC = networks['classifier']
-    netD = networks['discriminator']
     result_dir = options['result_dir']
-    image_size = options['image_size']
-    latent_size = options['latent_size']
-    output_frame_count = options['counterfactual_frame_count']
-    speed = options['speed']
-    momentum_mu = options['momentum_mu']
-    max_iters = options['counterfactual_max_iters']
     result_dir = options['result_dir']
 
-
-    # For MNIST, SVHN
-    # Generate a 10x10 square visualization
-    N = dataloader.num_classes
+    # Generate a K by K square grid of examples, one column per class
+    K = dataloader.num_classes
     images = [[] for _ in range(N)]
     for img_batch, label_batch, _ in dataloader:
         for img, label in zip(img_batch, label_batch):
@@ -293,6 +43,43 @@ def generate_grid(networks, dataloader, **options):
     images = np.array(images).transpose((0,2,3,1))
     start_class = 0
     video_filename = make_video_filename(result_dir, dataloader, start_class, start_class, label_type='grid')
+    # Save the images in npy format to re-load as training data
+    trajectory_filename = video_filename.replace('.mjpeg', '.npy')
+    np.save(trajectory_filename, images)
+    # Save the images in jpg format to display to the user
+    imutil.show(images, filename=video_filename.replace('.mjpeg', '.jpg'))
+
+
+# Generates 'counterfactual' images for each class, by gradient descent of the class
+def generate_comparison(networks, dataloader, **options):
+    for net in networks:
+        networks[net].eval()
+    netG = networks['generator']
+    netD = networks['discriminator']
+    result_dir = options['result_dir']
+    image_size = options['image_size']
+    latent_size = options['latent_size']
+    output_frame_count = options['counterfactual_frame_count']
+    speed = options['speed']
+    momentum_mu = options['momentum_mu']
+    max_iters = options['counterfactual_max_iters']
+    result_dir = options['result_dir']
+
+    K = dataloader.num_classes
+
+    batches = []
+    for class_idx in range(K):
+        img_batch = generate_images_for_class(networks, dataloader, class_idx, **options)
+        batches.append(img_batch)
+
+    images = []
+    for i in range(K):
+        for batch in batches:
+            images.append(batch[i])
+
+    images = np.array(images).transpose((0,2,3,1))
+    dummy_class = 0
+    video_filename = make_video_filename(result_dir, dataloader, dummy_class, dummy_class, label_type='grid')
 
     # Save the images in npy format to re-load as training data
     trajectory_filename = video_filename.replace('.mjpeg', '.npy')
@@ -302,83 +89,63 @@ def generate_grid(networks, dataloader, **options):
     imutil.show(images, filename=video_filename.replace('.mjpeg', '.jpg'))
 
 
-def select_uncertain_example(dataloader, netE, netC, pool_size=100, reverse=False):
-    print("Performing uncertainty sampling with dataloader {}".format(dataloader))
-    images = []
-    certainties = []
-    most_likely_classes = []
-    least_likely_classes = []
+def generate_images_for_class(networks, dataloader, class_idx, **options):
+    netG = networks['generator']
+    netD = networks['discriminator']
+    result_dir = options['result_dir']
+    image_size = options['image_size']
+    latent_size = options['latent_size']
+    output_frame_count = options['counterfactual_frame_count']
+    speed = options['speed']
+    momentum_mu = options['momentum_mu']
+    max_iters = options['counterfactual_max_iters']
+    result_dir = options['result_dir']
 
-    for i, (img, label) in enumerate(dataloader):
-        images.append(img)
-        z = netE(Variable(img))
-        preds = torch.exp(netC(z))
-        certainty, class_idx = preds.max(1)
-        uncertainty, min_idx = preds.min(1)
-        certainties.append(to_np(certainty)[0])
-        most_likely_classes.append(to_np(class_idx)[0])
-        least_likely_classes.append(to_np(min_idx)[0])
-        if i > pool_size:
-            break
-    if reverse:
-        idx = np.argmax(certainties)
-    else:
-        idx = np.argmin(certainties)
-    selected_image = images[idx]
-    most_likely_class = most_likely_classes[idx]
-    least_likely_class = least_likely_classes[idx]
-    selected_certainty = certainties[idx]
-    return most_likely_class, least_likely_class, selected_certainty, selected_image
+    # Start with K random points
+    K = dataloader.num_classes
+    z = gen_noise(K, latent_size)
+    z = Variable(z, requires_grad=True).cuda()
 
-
-# Selects a class different from the start class
-def random_target_class(dataloader, start_class):
-    target_class = random.randint(0, dataloader.num_classes - 2)
-    if start_class <= target_class:
-        target_class += 1
-    return target_class
-
-
-def generate_z_trajectory(z, target_class, netC, netE, netG, dataloader,
-        speed=.01, momentum_mu=.96, max_iters=100, spherical=True):
-    # Generate z_trajectory
-    z_trajectory = []
-    z_trajectory.append(to_np(z))  # initial point
-    momentum = Variable(torch.zeros(z.size())).cuda()
-    target_label = torch.LongTensor(1)
-    target_label[:] = int(target_class)
+    # Move them so their labels match target_label
+    target_label = torch.LongTensor(K)
+    target_label[:] = class_idx
     target_label = Variable(target_label).cuda()
-    original_z = z.clone()
-    max_iters = 250
-    momentum_mu = .9999
-    speed = .02
+
+    maxval = None  # So speed=.1 works for any classifier
+
     for i in range(max_iters):
-        net_y = netC(netE(netG(z)))
-        #net_y = netC(z)
+        images = netG(z)
+        net_y = netD(images)
+        if maxval is None:
+            maxval = net_y.max()
+        net_y /= maxval
         preds = softmax(net_y)
 
         predicted_class = to_np(preds.max(1)[1])[0]
         pred_confidence = to_np(preds.max(1)[0])[0]
-        z_trajectory.append(to_np(z))
         predicted_class_name = dataloader.lab_conv.labels[predicted_class]
         print("Class: {} ({:.3f} confidence). Target class {}".format(
-            predicted_class_name, pred_confidence, target_class))
+            predicted_class_name, pred_confidence, class_idx))
 
         cf_loss = nll_loss(log_softmax(net_y), target_label)
 
-        # Distance in latent space from original point
-        #cf_loss += .00001 * torch.sum((z - original_z) ** 2)
-
         dc_dz = autograd.grad(cf_loss, z, cf_loss, retain_graph=True)[0]
-        momentum -= dc_dz * speed
-        z += momentum
-        momentum *= momentum_mu
-        if spherical:
-            # Project z to the unit sphere
-            z /= torch.sqrt(torch.mul(z, z).sum())
-        if predicted_class == target_class and pred_confidence > .9:
-            break
-    return z_trajectory
+        z -= dc_dz * speed
+        z /= torch.sqrt(torch.mul(z, z).sum())
+    return images.data.cpu().numpy()
+    
+    
+def gen_noise(K, latent_size):
+    noise = torch.zeros((K, latent_size))
+    noise.normal_(0, 1)
+    noise = clamp_to_unit_sphere(noise)
+    return noise
+
+
+def clamp_to_unit_sphere(x):
+    norm = torch.norm(x, p=2, dim=1)
+    norm = norm.expand(1, x.size()[0])
+    return torch.mul(x, 1/norm.t())
 
 
 # Trajectories are written to result_dir/trajectories/
@@ -394,24 +161,3 @@ def make_video_filename(result_dir, dataloader, start_class, target_class, label
         print("Creating trajectories directory {}".format(path))
         os.mkdir(path)
     return video_filename
-
-
-def sample_trajectory(zt, output_sample_count):
-    distances = np.array([np.linalg.norm(zt[i+1] - zt[i]) for i in range(len(zt) - 1)])
-    total_distance = sum(distances)
-    if total_distance == 0:
-        print("Warning: Trajectory has zero length")
-        return [zt[0]] * output_sample_count
-    samples = []
-    for i in range(output_sample_count):
-        sample_distance = i * (total_distance / output_sample_count)
-        samples.append(get_sample_point(zt, sample_distance, distances))
-    return samples
-        
-
-def get_sample_point(zt, sample_distance, distances):
-    for i in range(len(zt)):
-        theta = (sample_distance - sum(distances[:i])) / distances[i]
-        if theta <= 1:
-            return (1 - theta) * zt[i] + theta * zt[i+1]
-    assert False
--- experiments/generate_counterfactual.py
+++ experiments/generate_counterfactual.py
@@ -13,7 +13,7 @@ parser.add_argument('--result_dir', required=True, help='Output directory for im
 
 # Other options can change with every run
 parser.add_argument('--batch_size', type=int, default=64, help='Batch size [default: 64]')
-parser.add_argument('--fold', type=str, default='validation', help='Fold [default: validation]')
+parser.add_argument('--fold', type=str, default='train', help='Fold [default: train]')
 parser.add_argument('--start_class', type=str, help='Name of starting class (random by default)')
 parser.add_argument('--target_class', type=str, help='Name target class (random by default)')
 parser.add_argument('--counterfactual_frame_count', default=60, type=int,
@@ -39,13 +39,12 @@ from options import load_options
 
 options = load_options(options)
 
-# Active Learning trajectories can only be single examples
-# TODO: enable batches
-options['batch_size'] = 1
 
-# NOTE: This dataloader should sample from a held-out pool of unlabeled examples used only for active learning
-# In these experiments we use the 'validation' fold for this purpose
 dataloader = CustomDataloader(**options)
+
+# Generate K x K grids of examples (to visualize K classes)
+options['batch_size'] = dataloader.num_classes
+
 networks = build_networks(dataloader.num_classes, **options)
 
 for i in range(options['count']):
