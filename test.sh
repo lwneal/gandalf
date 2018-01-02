8dbd8feef365379467fa794e989bcc94a1caaf1c
Larry Neal <nealla@lwneal.com>
2018-01-02 15:05:52 -0800

    Fix test.sh

--- test.sh
+++ test.sh
@@ -15,16 +15,15 @@ function make_sure_directory_exists() {
 make_sure_directory_exists /mnt/results
 make_sure_directory_exists /mnt/data
 
+rm -r /mnt/results/svhn_32x32_test
+
 python datasets/download_mnist.py
 
-experiments/train_aac.py \
-    --result_dir /mnt/results/mnist_28x28 \
-    --dataset /mnt/data/mnist.dataset \
-    --image_size 28 \
+experiments/train_model.py \
+    --result_dir /mnt/results/svhn_32x32_test \
+    --dataset /mnt/data/svhn.dataset \
+    --image_size 32 \
     --latent_size 10 \
-    --encoder encoder28 \
-    --generator generator28 \
-    --discriminator discriminator28 \
     --epochs 1
 
 experiments/example.py --result_dir /mnt/results/mnist_28x28
